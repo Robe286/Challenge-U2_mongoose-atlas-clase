a8d0730214d7acab0e251c7bf4e2536a4dfd9468
Roberto Blasco~ <roberza2202@gmail.com>
2025-05-26 11:13:04 +0200

    ruta get

--- routes/users.js
+++ routes/users.js
@@ -13,9 +13,20 @@ router.post('/create', async (req, res) => {
     } catch {
         res
             .status(500)
-            .send({message: 'there was a problem trying to create de user'});
+            .send({message: 'there was a problem trying to create the user'});
     }
 });
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.status(201).send(users);
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({message: 'there was a problem trying to get the users'});
+    }
+})
 
 module.exports = router;
 
